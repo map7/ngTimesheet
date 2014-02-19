c0c7c95d8ae38ad59a4c9270d6ccaa8d523538f8
Michael Pope <michael@dtcorp.com.au>
2014-02-19 14:32:06 +1100

    Display a message box for timesheet and capture info when posting

--- myApp.js
+++ myApp.js
@@ -3,9 +3,13 @@ var myApp = angular.module('myApp', ['timer']);
 myApp.directive('timesheet', function() {
   return {
       restrict: 'E',
-      templateUrl: 'timesheet.html',
-	  controller: function($scope){
+	  scope: {
+		  postUrl: '@'
+	  },
+	  controller: function($scope, $element, $attrs, $transclude){
 		  $scope.timerRunning = true;
+		  $scope.toPost = false;
+		  $scope.time = null;
 		  
 		  $scope.startTimer = function(){
 			  $scope.$broadcast('timer-start');
@@ -15,12 +19,20 @@ myApp.directive('timesheet', function() {
 		  $scope.stopTimer = function(){
 			  $scope.$broadcast('timer-stop');
 			  $scope.timerRunning = false;
+			  $scope.toPost = true;
 		  };
 
+		  $scope.postTimesheet = function(){
+			  debugger
+			  console.log('Post ', this.message, ' to ', this.postUrl)
+		  }
+
 		  $scope.$on('timer-stopped', function (event, data){
+			  this.time = data;
 			  console.log('Timer Stopped - data = ', data);
 		  });
-	  }
+	  },
+      templateUrl: 'timesheet.html'
 
   };
 });
