68392bc34559a070b0471e5d361d2df90b83feaf
Michael Pope <michael@dtcorp.com.au>
2014-02-21 09:31:30 +1100

    Start setting up karma

---
+++ test/e2e/post_message_spec.js
@@ -0,0 +1,9 @@
+// Scenarios relating to the posting of the timesheets
+
+describe('Post timesheet', function(){
+	it("shows message", function(){
+		browser().navigateTo('/examples/directive.html')
+		
+	});
+
+});
