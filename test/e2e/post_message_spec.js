7d5fe572e4631eba9efaff454923e500446e80e6
Michael Pope <michael@dtcorp.com.au>
2014-02-21 10:27:01 +1100

    Test showing post message

--- test/e2e/post_message_spec.js
+++ test/e2e/post_message_spec.js
@@ -3,7 +3,9 @@
 describe('Post timesheet', function(){
 	it("shows message", function(){
 		browser().navigateTo('/examples/directive.html')
-		
+		expect(element('#message:visible').count()).toBe(0) // Make sure message is hidden
+		element('#stop').click();
+		expect(element('#message:visible').count()).toBe(1) // Message should be visible
 	});
 
 });
